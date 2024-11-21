49a815a735945fd7975e5626d64bb1c84ed54199
Ayushi Agarwal <ayushiagarwal969@gmail.com>
2024-11-22 03:28:32 +0530

    changes

---
+++ backend/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb+srv://ayushiagarwal969:ayu06@cluster0.mfep6.mongodb.net/'
+
+mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');
+    
+}).catch((err) => {
+    console.log(err);
+    
+});
+
+module.exports = mongoose;
--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -2,216 +2,114 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div class="bg-black">
       <>
-  {/* Testimonials */}
-  <div className="bg-black">
-    <div className="max-w-7xl px-4 lg:px-6 lg:px-8 py-12 lg:py-24 mx-auto">
-      <blockquote className="max-w-4xl mx-auto">
-        <p className="mb-6 md:text-lg">
-          <span className="font-semibold text-lime-400">Philip,</span>{" "}
-          <span className="text-neutral-500">CEO at Day Spa</span>
-        </p>
-        <p className="text-xl sm:text-2xl md:text-3xl md:leading-normal text-white">
-          I'm absolutely floored by the level of care and attention to detail
-          the team at Preline have put into this project and for one can
-          guarantee that we will be a return customer.
-        </p>
-        <footer className="mt-6 md:mt-10">
-          <div className="border-neutral-700">
-            <button
-              type="button"
-              className="group inline-flex items-center gap-x-3 text-neutral-400 text-sm focus:outline-none disabled:opacity-50 disabled:pointer-events-none"
-            >
-              <span className="size-8 md:size-10 flex flex-col justify-center items-center bg-white text-black rounded-full group-hover:bg-lime-400 group-focus:bg-lime-400">
-                <svg
-                  className="shrink-0 size-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={16}
-                  height={16}
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
-                </svg>
-              </span>
-              Watch the Video
-            </button>
+        {/* Features */}
+        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+          {/* Grid */}
+          <div className="md:grid md:grid-cols-2 md:items-center md:gap-12 xl:gap-32">
+            <div>
+              <img
+                className="rounded-xl mt-10"
+                src="https://i.pinimg.com/originals/1c/a7/49/1ca74946ed770bb635e4de4711bd861f.gif"
+                alt="Features Image"
+              />
+            </div>
+            {/* End Col */}
+            <div className="mt-5 sm:mt-10 lg:mt-0">
+              <div className="space-y-6 sm:space-y-8">
+                {/* Title */}
+                <div className="space-y-2 md:space-y-4">
+                  <h2 className="font-bold text-3xl lg:text-4xl text-white">
+                   Welcome to Library<span class="text-gray-400">HUB</span>
+                  </h2>
+                  <p className="text-gray-500">
+                     Your one-stop destination for discovering a wide range of libraries across multiple programming languages and frameworks.
+                  </p>
+                  <p className="text-gray-500">Our platform offers an extensive collection of libraries for various languages and frameworks,catering to your diverse development needs.</p>
+                </div>
+                {/* End Title */}
+                {/* List */}
+                <ul className="space-y-2 sm:space-y-4">
+                  <li className="flex gap-x-3">
+                    <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
+                      <svg
+                        className="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12" />
+                      </svg>
+                    </span>
+                    <div className="grow">
+                      <span className="text-sm sm:text-base text-gray-500">
+                        <span className="font-bold">Centralized Resource</span> Hub
+                      </span>
+                    </div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
+                      <svg
+                        className="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12" />
+                      </svg>
+                    </span>
+                    <div className="grow">
+                      <span className="text-sm sm:text-base text-gray-500">
+                        Enhance Developer <span className="font-bold">Productivity</span>
+                      </span>
+                    </div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <span className="mt-0.5 size-5 flex justify-center items-center rounded-full bg-blue-50 text-blue-600">
+                      <svg
+                        className="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12" />
+                      </svg>
+                    </span>
+                    <div className="grow">
+                      <span className="text-sm sm:text-base text-gray-500">
+                        Save Development Time
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+                {/* End List */}
+              </div>
+            </div>
+            {/* End Col */}
           </div>
-        </footer>
-      </blockquote>
-    </div>
-  </div>
-  {/* End Testimonials */}
-</>
-<>
-  {/* Image Grid */}
-  <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1656618724305-a4257e46e847?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
+          {/* End Grid */}
         </div>
-      </div>
-    </a>
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1616427592793-67b858804534?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
-        </div>
-      </div>
-    </a>
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
-        </div>
-      </div>
-    </a>
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1707760696486-2a2cd7e0b6a6?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
-        </div>
-      </div>
-    </a>
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1585159812596-fac104f2f069?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
-        </div>
-      </div>
-    </a>
-    <a className="group block relative overflow-hidden rounded-lg" href="#">
-      <img
-        className="w-full size-40 object-cover bg-gray-100 rounded-lg"
-        src="https://images.unsplash.com/photo-1654131300276-db70adf4f85d?q=80&w=320&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Project"
-      />
-      <div className="absolute bottom-1 end-1 opacity-0 group-hover:opacity-100 transition">
-        <div className="flex items-center gap-x-1 py-1 px-2 bg-white border border-gray-200 text-gray-800 rounded-lg">
-          <svg
-            className="shrink-0 size-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx={11} cy={11} r={8} />
-            <path d="m21 21-4.3-4.3" />
-          </svg>
-          <span className="text-xs">View</span>
-        </div>
-      </div>
-    </a>
-  </div>
-  {/* End Image Grid */}
-</>
+        {/* End Features */}
+      </>
 
     </div>
   )
