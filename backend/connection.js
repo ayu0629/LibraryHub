fb0d81b20fd79e6c7d8a973b9e49b82361d59d05
ayu0629 <ayushiagarwal2906@gmail.com>
2024-11-25 23:29:24 +0530

    changes

--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://ayushiagarwal969:ayu06@cluster0.mfep6.mongodb.net/'
-
-mongoose.connect(url)
+mongoose.connect(process.env.MONGO_URL)
 .then((result) => {
     console.log('database connected');
     
